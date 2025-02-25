4c82ff76377d964c30ff42295719f466da3759ea
Daniel Kesselberg <mail@danielkesselberg.de>
2025-02-25 21:46:09 +0100

    chore: add help text for command, use shorter description
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- lib/Command/Generate.php
+++ lib/Command/Generate.php
@@ -11,6 +11,7 @@ namespace OCA\UserUsageReport\Command;
 use OCA\UserUsageReport\Reports\AllUsers;
 use OCA\UserUsageReport\Reports\SingleUser;
 use OCP\IUserManager;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -18,22 +19,17 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\StreamOutput;
 
+#[AsCommand(
+	name: 'usage-report:generate',
+	description: 'Generate usage report',
+	hidden: false,
+)]
 class Generate extends Command {
 
-	/** @var SingleUser */
-	protected $single;
+	protected SingleUser $single;
+	protected AllUsers $all;
+	protected IUserManager $userManager;
 
-	/** @var AllUsers */
-	protected $all;
-
-	/** @var IUserManager */
-	protected $userManager;
-
-	/**
-	 * @param SingleUser $single
-	 * @param AllUsers $all
-	 * @param IUserManager $userManager
-	 */
 	public function __construct(SingleUser $single, AllUsers $all, IUserManager $userManager) {
 		parent::__construct();
 
@@ -44,10 +40,12 @@ class Generate extends Command {
 
 	protected function configure(): void {
 		$this
-			->setName('usage-report:generate')
-			->setDescription(
-				'Prints a CSV entry with some usage information of the user:' . "\n"
-				. 'userId,date,assignedQuota,usedQuota,numFiles,numShares,numUploads,numDownloads' . "\n"
+			->setHelp(
+				'Print usage information for a given or all users.' . "\n"
+				. "\n"
+				. "Example (with --verbose to show the header):\n"
+				. "\n"
+				. '"user-id","date as \'c\'","assigned quota (5 GB)","used quota (500 MB)",number of files,number of shares,number of uploads,number of downloads' . "\n"
 				. '"admin","2017-09-18T09:00:01+00:00",5368709120,786432000,1024,23,1400,5678'
 			)
 			->addArgument(
